a752ddb94bcc8173dcdc125e835fa7a28d0db87e
siddharth  sevakram choure <lnccbtc21049@lnctu.ac.in>
2025-05-13 10:32:25 +0000

    update

--- main.js
+++ main.js
@@ -1,4 +1,41 @@
+// Prevent GPU/DBus issues in headless environments
+process.env.DBUS_SESSION_BUS_ADDRESS = 'unix:path=/dev/null';
+process.env.CHROME_DEVEL_SANDBOX = '/tmp/chrome-devel-sandbox';
+process.env.XDG_RUNTIME_DIR = '/tmp';
+process.env.MEMORY_PRESSURE_THRESHOLD_MB = '4096';
+process.env.ELECTRON_ENABLE_STACK_DUMPING = 'true';
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true';
+process.env.XDG_CONFIG_HOME = '/tmp';
+process.env.XDG_CACHE_HOME = '/tmp';
+process.env.XDG_DATA_HOME = '/tmp';
+process.env.QT_QPA_PLATFORM = 'offscreen';
+process.env.XDG_SESSION_TYPE = 'x11';
+process.env.XDG_SESSION_DESKTOP = 'x11';
+process.env.XAUTHORITY = '/tmp/.Xauthority';
+process.env.TMPDIR = '/tmp';
+process.env['ELECTRON_RUN_AS_NODE'] = '1';
+process.env['ELECTRON_FORCE_WINDOW_MENU_BAR'] = '1';
+process.env['ELECTRON_DISABLE_GPU_SANDBOX'] = '1';
+process.env['ELECTRON_OZONE_PLATFORM_HINT'] = 'x11';
+process.env['ELECTRON_ENABLE_LOGGING'] = 'true';
+process.env['ELECTRON_NO_ATTACH_CONSOLE'] = '1';
+process.env['ELECTRON_DISABLE_SANDBOX'] = '1';
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+process.env['ELECTRON_DISABLE_GPU'] = '1';
+process.env['ELECTRON_DISABLE_HARDWARE_ACCELERATION'] = '1';
+process.env['ELECTRON_DISABLE_SOFTWARE_RASTERIZER'] = '1';
+process.env['ELECTRON_DISABLE_VIZ_DISPLAY_COMPOSITOR'] = '1';
+process.env['ELECTRON_DISABLE_DEV_SHM_USAGE'] = '0';
+process.env['ELECTRON_CHROMIUM_SHM_DIR'] = '/dev/shm';
+
 const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+
+app.disableHardwareAcceleration();
+app.commandLine.appendSwitch('no-sandbox');
+app.commandLine.appendSwitch('disable-gpu');
+app.commandLine.appendSwitch('disable-software-rasterizer');
+app.commandLine.appendSwitch('disable-features', 'VizDisplayCompositor');
+
 const path = require('path');
 require('dotenv').config();
 
